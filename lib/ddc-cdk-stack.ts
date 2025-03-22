c8a46d7308ec6d9e633f0e2e65d1915e485d79a3
declspecl <gavind2559@gmail.com>
2025-03-22 14:07:01 -0400

    create table

--- lib/ddc-cdk-stack.ts
+++ lib/ddc-cdk-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ddb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 import { LambdaPipeline } from './lambda-pipeline';
 
@@ -13,6 +14,12 @@ export class DdcCdkStack extends cdk.Stack {
       githubRepo: "lambda",
       githubBranch: "main",
       timeout: cdk.Duration.minutes(10),
-    })
+    });
+
+    const table = new ddb.Table(this, 'DDCTable', {
+      partitionKey: { name: 'pk', type: ddb.AttributeType.STRING },
+      tableName: 'DDCTable',
+      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+    });
   }
 }
